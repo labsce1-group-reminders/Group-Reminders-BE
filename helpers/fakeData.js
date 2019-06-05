4ec6a9369d0c3e29917fc559ccf89f82a3c645e1
sibhat <sibhat.temelso@gmail.com>
2019-06-04 22:09:08 -0700

    Update fakeData module to match current model schema.

--- helpers/fakeData.js
+++ helpers/fakeData.js
@@ -6,32 +6,65 @@ const userSeeds = 5;
 const seriesSeeds = 20;
 const memberSeeds = 500;
 const messageSeeds = 20;
+const classSeeds = 10;
 
 module.exports = {
   createFakeUsers,
   createFakeTrainingSeries,
-  createFakeTeamMembers,
-  createFakeMessages
+  createFakeClassMembers,
+  createFakeMessages,
+  createFakeClasses,
+  createFakeOrganizations
 };
 
 function createFakeUsers() {
-  const newUsers = [];
-  const fakeUser = () => ({
-    name: faker.name.findName(),
+  const newAdminUsers = [];
+  const newManagerUsers = [];
+  const fakeUser = (type) => ({
+    firstName: faker.name.findName(),
+    lastName: faker.name.findName(),
     email: faker.internet.email(),
     notifications_sent: 0,
-    account_type_id: faker.random.number({ min: 1, max: 3 })
+    country: type ? "" : faker.address.country(),
+    is_admin: type,
+    is_manager: type,
   });
   for (let i = 0; i < userSeeds; i++) {
-    newUsers.push(fakeUser());
+    newAdminUsers.push(fakeUser(true));
   }
-  return newUsers;
+  for (let i = 0; i < userSeeds; i++) {
+    newManagerUsers.push(fakeUser(false));
+  }
+  return [...newAdminUsers, ...newManagerUsers];
+}
+function createFakeClasses() {
+  const newClasses = [];
+  const fakeClasses = () => ({
+    title: faker.commerce.department(),
+    user_id: faker.random.number({ min: 1, max: classSeeds }),
+  });
+  for (let i = 0; i < classSeeds; i++) {
+    newClasses.push(fakeClasses());
+  }
+  return newClasses;
+}
+function createFakeOrganizations() {
+  const newOrganizations = [];
+  const fakeClasses = () => ({
+    name: faker.commerce.department(),
+    user_id: faker.random.number({ min: 1, max: userSeeds }),
+  });
+  for (let i = 0; i < userSeeds; i++) {
+    newOrganizations.push(fakeClasses());
+  }
+  return newOrganizations;
 }
 
 function createFakeTrainingSeries() {
   const newSeries = [];
   const fakeSeries = () => ({
     title: faker.company.catchPhrase(),
+    country: faker.address.country(),
     user_id: faker.random.number({
       min: 1,
       max: userSeeds
@@ -45,24 +78,22 @@ function createFakeTrainingSeries() {
   return newSeries;
 }
 
-function createFakeTeamMembers() {
-  const newTeamMembers = [];
+function createFakeClassMembers() {
+  const newClassMembers = [];
   const fakeTeamMember = () => ({
     first_name: faker.name.firstName(),
     last_name: faker.name.lastName(),
-    job_description: faker.commerce.department(),
     email: faker.internet.email(),
     phone_number: faker.phone.phoneNumber(),
     slack_uuid: uuidv4(),
     user_id: faker.random.number({ min: 1, max: userSeeds }),
-    manager_id: faker.random.number({ min: 1, max: memberSeeds }),
-    mentor_id: faker.random.number({ min: 1, max: memberSeeds })
+    class_id: faker.random.number({ min: 1, max: classSeeds }),
   });
   for (let i = 0; i < memberSeeds; i++) {
-    newTeamMembers.push(fakeTeamMember());
+    newClassMembers.push(fakeTeamMember());
   }
 
-  return newTeamMembers;
+  return newClassMembers;
 }
 
 function createFakeMessages() {
@@ -71,12 +102,14 @@ function createFakeMessages() {
     subject: faker.company.catchPhrase(),
     body: faker.company.bs(),
     link: faker.internet.url(),
+    status: faker.random.number({min: 0, max: 2}),
     training_series_id: faker.random.number({
       min: 1,
       max: seriesSeeds
     }),
     for_manager: false,
-    for_mentor: false,
+    for_class: false,
+    for_class_member: true,
     days_from_start: faker.random.number({ min: 1, max: 100 })
   });
   for (let i = 0; i < messageSeeds; i++) {
