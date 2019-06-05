df8347326c6be61950912e195f263caf2a5bf505
sibhat <sibhat.temelso@gmail.com>
2019-06-04 22:11:33 -0700

    update seed files to match new/current model schema

--- models/seeds/0003-services.js
+++ models/seeds/0003-services.js
@@ -1,5 +1,5 @@
 exports.seed = function(knex, Promise) {
-  return knex("services").then(function() {
+  return knex("services").del().then(function() {
     return knex("services").insert([
       { name: "twilio" },
       { name: "sendgrid" },
