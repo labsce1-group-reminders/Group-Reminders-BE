11eb9440bcd24235fbbfff49d6d0564fee8c9c1f
sibhat <sibhat.temelso@gmail.com>
2019-06-06 17:11:19 -0700

    classMember controller include get all class members, create a class member, and [get, update, delete]

--- controllers/classMember.js
+++ controllers/classMember.js
@@ -2,7 +2,7 @@
 const router = require("express").Router();
 
 // Models
-const TeamMember = require("../models/db/teamMembers");
+const ClassMember = require("../models/db/classMembers");
 const Messages = require("../models/db/messages");
 const Notifications = require("../models/db/notifications");
 
@@ -10,14 +10,14 @@ const Notifications = require("../models/db/notifications");
 const arrayFlat = require("../helpers/arrayFlat");
 
 // Data validation
-const { teamMemberSchema } = require("../models/schemas");
+const { classMemberSchema } = require("../models/schemas");
 const validation = require("../middleware/dataValidation");
 
 router
   .route("/")
   .get(async (req, res) => {
     /**
-     * Get all Team Members associated with an authenticated user
+     * Get all Class Members
      *
      * @function
      * @param {Object} req - The Express request object
@@ -25,41 +25,36 @@ router
      * @returns {Object} - The Express response object
      */
 
-    // Destructure the authenticated User email off of res.locals
-    const { email } = res.locals.user;
+    // Get all Class Members from the database that are associated with the authenticated User
+    const classMembers = await ClassMember.get();
 
-    // Get all Team Members from the database that are associated with the authenticated User
-    const teamMembers = await TeamMember.find({
-      "u.email": email
-    });
-
-    // Return the found Team Members to client
-    return res.status(200).json({ teamMembers });
+    // Return the found Class Members to client
+    return res.status(200).json({ classMembers });
   })
-  .post(validation(teamMemberSchema), async (req, res) => {
+  .post(validation(classMemberSchema), async (req, res) => {
     /**
-     * Validate the request body against our Team Member schema and then Create
-     * a new Team Member
+     * Validate the request body against our Class Member schema and then Create
+     * a new Class Member
      *
      * @function
      * @param {Object} req - The Express request object
-     * @param {Object} req.body - The request body, which represents a new Team Member
+     * @param {Object} req.body - The request body, which represents a new Class Member
      * @param {Object} res - The Express response object
      * @returns {Object} - The Express response object
      */
 
-    // Add the new Team Member to the database
-    const newTeamMember = await TeamMember.add(req.body);
+    // Add the new Class Member to the database
+    const newClassMember = await ClassMember.add(req.body);
 
-    // Return the newly created Team Member to the client
-    return res.status(201).json({ newTeamMember });
+    // Return the newly created Class Member to the client
+    return res.status(201).json({ newClassMember });
   });
 
 router
   .route("/:id")
   .get(async (req, res) => {
     /**
-     * Get a specific Team Member by their ID
+     * Get a specific Class Member by their ID
      *
      * @function
      * @param {Object} req - The Express request object
@@ -71,28 +66,27 @@ router
     const { id } = req.params;
 
     // Destructure the authenticated User email off of res.locals
-    const { email } = res.locals.user;
+    // const { email } = res.locals.user;
 
-    // Get the Team Member associated with a user by ID
-    const teamMember = await TeamMember.find({
-      "tm.id": id,
-      "u.email": email
+    // Get the Class Member associated with a user by ID
+    const classMember = await ClassMember.find({
+      "cm.id": id,
     }).first();
 
-    // If teamMember is falsey, we can assume that Team Member doesn't exist
-    if (!teamMember) {
+    // If teamMember is falsy, we can assume that Class Member doesn't exist
+    if (!classMember) {
       return res.status(404).json({
-        message: "Sorry, but we couldnt find that team member!"
+        message: "Sorry, but we couldn't find that class member!"
       });
     }
 
     // Return the Team Member to the client
-    return res.status(200).json({ teamMember });
+    return res.status(200).json({ classMember });
   })
-  .put(validation(teamMemberSchema), async (req, res) => {
+  .patch(validation(classMemberSchema), async (req, res) => {
     /**
-     * Validate the request body against the Team Member schema, then update
-     * the specified Team Member in the database
+     * Validate the request body against the Class Member schema, then update
+     * the specified Class Member in the database
      *
      * @function
      * @param {Object} req - The Express request object
@@ -104,18 +98,18 @@ router
     // Destructure the ID off the request parameters
     const { id } = req.params;
 
-    // Update the specific team member in the database
-    const updatedTeamMember = await TeamMember.update(
-      { "tm.id": id },
+    // Update the specific Class member in the database
+    const updatedClassMember = await ClassMember.update(
+      { "cm.id": id },
       req.body
     );
 
     // Return the updated Team Member to the client
-    return res.status(200).json({ updatedTeamMember });
+    return res.status(200).json({ updatedClassMember });
   })
   .delete(async (req, res) => {
     /**
-     * Delete a specified Team Member
+     * Delete a specified Class Member
      *
      * @function
      * @param {Object} req - The Express request object
@@ -126,10 +120,10 @@ router
     // Destructure the ID off of the request object
     const { id } = req.params;
 
-    // Attempt to delete the specified Team Member from the database
-    const deleted = await TeamMember.remove({ "tm.id": id });
+    // Attempt to delete the specified Class Member from the database
+    const deleted = await ClassMember.remove({ "cm.id": id });
 
-    // If deleted is falsey, we can assume that there is no Team Member at that ID
+    // If deleted is falsy, we can assume that there is no Class Member at that ID
     if (!deleted) {
       return res.status(404).json({
         message: "The resource could not be found."
@@ -144,8 +138,8 @@ router
 
 router.delete("/:id/unassign/:ts_id", async (req, res) => {
   /**
-   * "Unassign" a specific Team Member from a specific Training Series.
-   * Ultimately, this deletes all pending notifications for that Team Member
+   * "Unassign" a specific Class Member from a specific Training Series.
+   * Ultimately, this deletes all pending notifications for that Class Member
    * that are associated with a particular Training Series.
    *
    * @function
@@ -167,7 +161,7 @@ router.delete("/:id/unassign/:ts_id", async (req, res) => {
   if (!messages.length) {
     return res.status(404).json({
       message:
-        "This Team Member doesn't have any messages for that Training Series."
+        "This Class Member doesn't have any messages for that Training Series."
     });
   }
 
@@ -199,7 +193,7 @@ router.delete("/:id/unassign/:ts_id", async (req, res) => {
   if (!totalDeleted.length) {
     return res.status(404).json({
       message:
-        "This Team Member doesn't have any Notifications for that Training Series."
+        "This Class Member doesn't have any Notifications for that Training Series."
     });
   }
 
